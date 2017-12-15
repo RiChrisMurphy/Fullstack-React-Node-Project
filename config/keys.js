885be459a878819ccfc1ad4fbcea9a9d2b7ef368
ChrisCreatorArcade <chris@creatorarcade.com>
2017-12-15 14:52:00 -0800

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// figure what set of credentials to return
+if (process.env.NODE_ENV === 'production') {
+ //return prod keys
+ module.exports = require('./prod');
+} else {
+ // we are in dev - return dev keys!!
+ module.exports = require('./dev');
+}
