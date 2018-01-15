209edd4a27eb77da305cacb2c602f5a912b20c09
RiChrisMurphy <rcmurphy@ucdavis.edu>
2018-01-15 10:19:17 -0800

    surveys to the database

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,6 +8,9 @@ const Mailer = require('../services/Mailer');
 const Survey = mongoose.model('surveys');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 module.exports = app => {
+ app.get('/api/surveys/:surveyId/:choice', (req, res) => {
+  res.send('Thanks for voting!');
+ });
  app.post('/api/surveys/webhooks', (req, res) => {
   const p = new Path('/api/surveys/:surveyId/:choice');
   const events = _.chain(req.body)
@@ -23,9 +26,22 @@ module.exports = app => {
    })
    .compact()
    .uniqBy('email', 'surveyId')
+   .each(({ surveyId, email, choice }) => {
+    Survey.updateOne(
+     {
+      _id: surveyId,
+      recipients: {
+       $elemMatch: { email: email, responded: false }
+      }
+     },
+     {
+      $inc: { [choice]: 1 },
+      $set: { 'recipients.$.responded': true },
+      lastResponded: new Date()
+     }
+    ).exec();
+   })
    .value();
-
-  console.log(events);
  });
 
  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
