c3a39527d9451aa65551ddfc4729c50b5e669376
RiChrisMurphy <rcmurphy@ucdavis.edu>
2018-01-19 15:17:01 -0800

    removed axios from window object also development sendgrid webhooks work, but production isn't atm

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import axios from 'axios';
-window.axios = axios;
+//window.axios = axios;
 //development only axios helpers
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 ReactDOM.render(
